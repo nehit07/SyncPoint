6073e206ae560c27d2b46de313cf0919283e6302
kunjk <kunjkoradiya09@gmail.com>
2024-02-09 15:51:45 +0530

    CMT

--- syncpoint/src/App.jsx
+++ syncpoint/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>SyncPoint</h1>
+      <h1> SyncPoint </h1>
     
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
