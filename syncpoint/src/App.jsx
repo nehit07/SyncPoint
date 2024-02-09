a20a8b53ec53198a9d415340efd2c2c499861190
Yash <yashaghoda@gmail.com>
2024-02-09 15:49:33 +0530

    change

--- syncpoint/src/App.jsx
+++ syncpoint/src/App.jsx
@@ -17,7 +17,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-    
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -27,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Hello WOrld
       </p>
     </div>
   )
